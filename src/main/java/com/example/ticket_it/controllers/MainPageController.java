4fe7f70542a0768aa58153fe8528f33b9a6b9042
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2023-01-05 13:27:08 +0100

    start

--- src/main/java/com/example/ticket_it/controllers/MainPageController.java
+++ src/main/java/com/example/ticket_it/controllers/MainPageController.java
@@ -1,4 +1,19 @@
 package com.example.ticket_it.controllers;
 
+import com.example.ticket_it.services.MainPageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainPageController {
+
+    @Autowired
+    MainPageService mainPageService;
+
+    @RequestMapping("/")
+    public String mainPage(Model model) {
+        return "Hello!";
+    }
 }
--- src/main/java/com/example/ticket_it/services/MainPageService.java
+++ src/main/java/com/example/ticket_it/services/MainPageService.java
@@ -1,4 +1,7 @@
 package com.example.ticket_it.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MainPageService {
 }
