d6f9825a7ed37b590e1c90f4bfeaa6d6e25351e5
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2023-01-05 14:22:59 +0100

    Classes

--- src/main/java/com/example/ticket_it/Seat.java
+++ src/main/java/com/example/ticket_it/Seat.java
@@ -1,4 +1,13 @@
 package com.example.ticket_it;
 
 public class Seat {
+    private final int seatNumber;
+    private final int rowNumber;
+    private final int sectorNumber;
+
+    public Seat(int seatNumber, int rowNumber, int sectorNumber) {
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+        this.sectorNumber = sectorNumber;
+    }
 }
--- src/main/java/com/example/ticket_it/User.java
+++ src/main/java/com/example/ticket_it/User.java
@@ -1,4 +1,19 @@
 package com.example.ticket_it;
 
 public class User {
+    private final int userID;
+    private final String name;
+    private final String surname;
+    private String email;
+    private final int personalID;
+    private int bankBalance;
+
+    public User(int userID, String name, String surname, String email, int personalID, int bankBalance) {
+        this.userID = userID;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.personalID = personalID;
+        this.bankBalance = bankBalance;
+    }
 }
