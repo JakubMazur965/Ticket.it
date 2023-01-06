d428bbeb0ab14fc2a6f5654b12605b5d602d297e
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2023-01-06 12:17:29 +0100

    Properties

--- src/main/java/com/example/ticket_it/components/SSHTunnel.java
+++ src/main/java/com/example/ticket_it/components/SSHTunnel.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 
 @Component
 @PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-local.properties")
 public class SSHTunnel {
 
     // db parameters - URL for DataBase
