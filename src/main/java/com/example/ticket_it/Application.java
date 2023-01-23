4472af387d459da6aa09f935bc6ac317aae13c2a
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2023-01-23 17:35:09 +0100

    ticket to buy - added

--- src/main/java/com/example/ticket_it/Application.java
+++ src/main/java/com/example/ticket_it/Application.java
@@ -1,14 +1,8 @@
 package com.example.ticket_it;
 
-import com.example.ticket_it.components.*;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import java.sql.*;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
